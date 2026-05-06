463d1de5d716c003b768bb8add132732d8356871
Glash007 <ashgsalesandmarketing@gmail.com>
2026-05-06 22:26:10 +0100

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+export const supabase = createClient(
+  'https://bxexzhxmjmdktjyufyec.supabase.co',
+  'sb_publishable_5gdqpfsUgYy5ExRKp33N0w_VEo3l4T0'
+);
