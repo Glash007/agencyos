66c210753a9e4199ff7eeffd95b0f64077f970f4
Glash007 <ashgsalesandmarketing@gmail.com>
2026-05-06 22:25:31 +0100

    Refactor ReactDOM rendering to use createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import 'reset.css';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
